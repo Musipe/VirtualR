40d82a9c01fda03769746c39843304a9add8c3c4
Musipe <mulwanasimon7@gmail.com>
2024-06-14 11:16:22 +0300

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR build tools{" "}
+        <span className="grad-bg text-transparent bg-clip-text">
+          for developers
+        </span>
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+      </p>
+      <div className="flex justify-center my-10">
+        <a href="#" className="grad-bg py-3 px-4 mx-3 rounded-md">
+          Start for free
+        </a>
+        <a href="#" className=" py-3 px-4 mx-3 rounded-md border">
+          Documentation
+        </a>
+      </div>
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          src={video1}
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        />
+        <video
+          autoPlay
+          loop
+          muted
+          src={video2}
+          className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
